b731f07e935e88bce5c8f2dd5566d21298002312
unknown <pa1g15@J03.ecs.soton.ac.uk>
2016-06-15 18:54:43 +0100

    create tabbed panel

--- src/marketFramework/Dashboard.java
+++ src/marketFramework/Dashboard.java
@@ -1,14 +1,61 @@
 package marketFramework;
 
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Dashboard extends JPanel {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+
+public class Dashboard extends JFrame {
+
+	private JPanel contentPane;
 
 	/**
-	 * Create the panel.
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Dashboard frame = new Dashboard();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
 	 */
 	public Dashboard() {
-
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+		
+		JPanel userPanel = new JPanel();
+		tabbedPane.addTab("Users", null, userPanel, null);
+		
+		JPanel auctionPanel = new JPanel();
+		tabbedPane.addTab("Auction", null, auctionPanel, null);
+		
+		JPanel historyPanel = new JPanel();
+		tabbedPane.addTab("History", null, historyPanel, null);
+		
+		JPanel timePanel = new JPanel();
+		contentPane.add(timePanel, BorderLayout.NORTH);
+		
+		JPanel buttonPanel = new JPanel();
+		contentPane.add(buttonPanel, BorderLayout.SOUTH);
 	}
 
 }
